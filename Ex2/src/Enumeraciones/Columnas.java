5e01e3adafb10571a7f4f33e394983c859d27ef0
Sebastian Salvadores <sebasalvadores@gmail.com>
2023-05-09 21:52:54 -0300

    Cuarto commit

---
+++ Ex2/src/Enumeraciones/Columnas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enumeraciones;
+
+/**
+ *
+ * @author Sebastian
+ */
+public enum Columnas {
+    A,
+    B,
+    C,
+    D,
+    E,
+    F;
+}
---
+++ Ex2/src/Enumeraciones/Nombres.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Enumeraciones;
+
+/**
+ *
+ * @author Sebastian
+ */
+public enum Nombres {
+    JUAN,
+    JOSE,
+    PEDRO,
+    MARIA,
+    LUCIA,
+    MARIO,
+    AGUSTINA,
+    LUIS,
+    JOSEFINA,
+    CANDELA,
+    SEBASTIAN,
+    CAMILA,
+    ENZO,
+    MICAELA;
+}
